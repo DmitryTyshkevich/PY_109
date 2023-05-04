da896e8001a58c32142aa3e9d4bfde5450ca324f
Dmitry_Tyshkevich <tihon2008199213@gmail.com>
2023-05-04 15:08:25 +0300

    Correcting the function description

--- parse_forever/async_parser.py
+++ parse_forever/async_parser.py
@@ -7,7 +7,7 @@ import time
 
 
 async def get_page_data(session, page):
-    '''Функция собирает данные со строницы'''
+    '''Функция собирает данные со страницы'''
     headers = {
         'Accept': 'text/html, application/xhtml+xml, application/xml;q=0.9,image/webp, */*;q=0.8',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
